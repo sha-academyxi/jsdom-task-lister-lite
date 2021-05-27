8f49528494327e0ec98bc76b0c6832ec6a34a5c1
SET mentor <setmentor_parttime02@academyxi.com>
2021-05-27 21:57:18 +1000

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    //form and relevant input fields
+    const newTaskForm = document.getElementById("create-task-form");
+    newTaskForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      const elem = document.createElement('li')
+      elem.textContent = document.getElementById("new-task-description").value
+      elem.contentEditable = true
+      elem.style.color = getBackgroundColorBasedOnPriority(document.getElementById('new-task-priority').value)
+
+      // add delete button
+      const deleteBtn = document.createElement('button')
+      deleteBtn.textContent = 'X'
+      deleteBtn.addEventListener('click', () => {
+        elem.remove()
+      })
+
+      elem.appendChild(deleteBtn)
+      document.getElementById("tasks").appendChild(elem)
+
+      newTaskForm.reset()
+    });
 });
+
+function getBackgroundColorBasedOnPriority(priority) {
+  switch (priority.toLowerCase()) {
+    case "high":
+      return "red"
+    case "medium":
+      return "yellow"
+    case "low":
+      return "green"
+  }
+}
